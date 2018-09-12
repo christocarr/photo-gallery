55acb9e088f2f90ea89383a471efe1e6c30a52ba
christocarr <christocarr@outlook.com>
2018-09-12 23:10:01 +0100

    Add Main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
-import Home from './components/Home';
+import Main from './components/Main';
 import Footer from './components/Footer';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Home />
+        <Main />
         <Footer />
       </div>
     );
