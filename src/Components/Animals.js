2d08fb817c2a7299bf01ad8f06193bdb2f64ed60
christocarr <christocarr@outlook.com>
2018-09-12 23:12:55 +0100

    Add heading and Photo set array

--- src/Components/Animals.js
+++ src/Components/Animals.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Gallery from 'react-photo-gallery';
 
+const PHOTO_SET = [
+  {
+    src: require('../images/cat1.jpg')
+  }
+]
+
 class Animals extends Component {
 
- componentDidMount() {
-  const PHOTO_SET = [
-    {
-      src: require('../images/cat1.jpg'),
-    }
-  ];
- }
+  state = {
+
+  }
   
   render() {
     return (
       <Route path="/animals" render={() => (
         <div>
+          <h2>Animals</h2>
           <Gallery photos={PHOTO_SET}/>
         </div>
       )} />
