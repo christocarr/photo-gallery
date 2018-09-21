9dce8ca3cb822836669aad0b5bf24dc4ba4d2007
Chris <christocarr@outlook.com>
2018-09-21 22:09:03 +0100

    Change index title

--- src/Components/People.js
+++ src/Components/People.js
@@ -4,7 +4,17 @@ import Gallery from 'react-photo-gallery';
 
 const PHOTO_SET = [
   {
-    // src: require('../images/')
+    src: '../images/people/mel/pregnant1920.jpg',
+    srcSet: [
+      // 'images/people/mel/pregnant320 320w',
+      // 'images/people/mel/pregnant640 640w',
+      // 'images/people/mel/pregnant1180 1180w',
+      // 'images/people/mel/pregnant1920 1920w',
+    ],
+    sizes: ['(min-width: 480px) 50vw,(min-width:1024px) 33.3vw, 100vw'],
+    width: 3,
+    height: 4,
+    key: '0'
   }
 ]
 
