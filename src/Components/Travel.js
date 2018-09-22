be98044f1f550382d1ca5518757b7c8fd4ab3f9b
Chris <christocarr@outlook.com>
2018-09-22 17:22:25 +0100

    Add new travel images

--- src/Components/Travel.js
+++ src/Components/Travel.js
@@ -1,25 +1,97 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
 
 const PHOTO_SET = [
   {
-    // src: require('../images/')
-  }
+    src: require('../images/travel/boat/boat1920.jpg'),
+    // srcSet: [
+    // ],
+    sizes: ['(min-width: 480px) 50vw,(min-width:1024px) 33.3vw, 100vw'],
+    width: 4.2,
+    height: 3,
+    key: '0',
+    alt: 'Boat in the Caribbean sunset'
+  },
+  {
+    src: require('../images/travel/market/market1920.jpg'),
+    // srcSet: [
+    // ],
+    sizes: ['(min-width: 480px) 50vw,(min-width:1024px) 33.3vw, 100vw'],
+    width: 5,
+    height: 3,
+    key: '1',
+    alt: 'Mexican market'
+  },
+  {
+    src: require('../images/travel/seascape/seascape1920.jpg'),
+    // srcSet: [
+    // ],
+    sizes: ['(min-width: 480px) 50vw,(min-width:1024px) 33.3vw, 100vw'],
+    width: 3,
+    height: 4,
+    key: '2',
+    alt: 'Caribbean nights'
+  },
+  {
+    src: require('../images/travel/tuk-tukdriver/tuk-tukdriver1920.jpg'),
+    // srcSet: [
+    // ],
+    sizes: ['(min-width: 480px) 50vw,(min-width:1024px) 33.3vw, 100vw'],
+    width: 3,
+    height: 4,
+    key: '3',
+    alt: 'Bangkok tuk-tuk driver'
+  },
 ]
 
 class Travel extends Component {
 
   state = {
+    currentImage: 0
+  }
 
+  openLightbox = (ev, obj) => {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });
+  } 
+  closeLightbox = () => {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious = () => {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext = () => {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
   }
   
   render() {
     return (
       <Route path="/travel" render={() => (
-        <div>
+        <div className="gallery-container">
           <h2>Travel</h2>
-          <Gallery photos={PHOTO_SET}/>
+          <Gallery 
+            photos={PHOTO_SET} 
+            onClick={this.openLightbox}
+            direction='row'
+          />
+          <Lightbox images={PHOTO_SET}
+            onClose={this.closeLightbox}
+            onClickPrev={this.gotoPrevious}
+            onClickNext={this.gotoNext}
+            currentImage={this.state.currentImage}
+            isOpen={this.state.lightboxIsOpen}
+          />
           <div className="bottom-nav">
             <Link to="/animals">
               <div className="animals">
