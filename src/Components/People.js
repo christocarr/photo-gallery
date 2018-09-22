3c7b2bb5c06d2a807285922601751f148fd07a56
Chris <christocarr@outlook.com>
2018-09-22 08:17:41 +0100

    Add react images

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,49 +1,80 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
 
 const PHOTO_SET = [
   {
-    src: '../images/people/mel/pregnant1920.jpg',
-    srcSet: [
-      // 'images/people/mel/pregnant320 320w',
-      // 'images/people/mel/pregnant640 640w',
-      // 'images/people/mel/pregnant1180 1180w',
-      // 'images/people/mel/pregnant1920 1920w',
-    ],
-    sizes: ['(min-width: 480px) 50vw,(min-width:1024px) 33.3vw, 100vw'],
+    src: require('../images/people/mel/pregnant1920.jpg'),
+    // srcSet: [
+    //   require('../images/people/mel/pregnant320 320w'),
+
+    // ],
+    // sizes: ['(min-width: 480px) 50vw,(min-width:1024px) 33.3vw, 100vw'],
     width: 3,
     height: 4,
     key: '0'
   }
 ]
 
+// 'images/people/mel/pregnant640 640w',
+// 'images/people/mel/pregnant1180 1180w',
+// 'images/people/mel/pregnant1920 1920w',
+
 class People extends Component {
 
   state = {
+    currentImage: 0
+  }
 
+  openLightbox = (ev, obj) => {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });
+  } 
+  closeLightbox = () => {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious = () => {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext = () => {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
   }
   
   render() {
     return (
       <Route path="/people" render={() => (
-        <div>
+        <div className="gallery-container">
           <h2>People</h2>
-          <Gallery photos={PHOTO_SET}/>
+          <Gallery photos={PHOTO_SET}
+           onClick={this.openLightbox}
+           direction='row'
+          />
+          <Lightbox images={PHOTO_SET}
+            onClose={this.closeLightbox}
+            onClickPrev={this.gotoPrevious}
+            onClickNext={this.gotoNext}
+            currentImage={this.state.currentImage}
+            isOpen={this.state.lightboxIsOpen}
+          />
           <div>
-            <Link to="/animals">
-              <div className="animals">
+            <Link to="/travel">
+              <div className="travel">
                 <h2>Travel</h2>
               </div>
             </Link>
-            <Link to="/animals">
-              <div className="animals">
-                <h2>Animals</h2>
-              </div>
-            </Link>
-            <Link to="/architecture">
-              <div className="architecture">
-                <h2>Architecture</h2>
+            <Link to="/people">
+              <div className="people">
+                <h2>People</h2>
               </div>
             </Link>
           </div>
