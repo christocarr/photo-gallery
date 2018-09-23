933127640343133f4208b4d58391f956eb4067f1
Chris <christocarr@outlook.com>
2018-09-23 10:56:07 +0100

    Add Link to header to return to home screen

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navigation from './Navigation';
-import MenuIcon from '../images/menu.svg'
+import MenuIcon from '../images/menu.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="header">
-    <h1>Chris Carr Photography</h1>
+    <Link to="/" exact className="header-link">
+      <h1>Chris Carr Photography</h1>
+    </Link>
     <img src={MenuIcon} className="menu-icon" alt="menu icon"/>
     <Navigation />
   </div>
