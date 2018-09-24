8237c171d2615d600dfe0bf849b940815cc38f6d
Chris <christocarr@outlook.com>
2018-09-24 19:55:37 +0100

    Add lorum ipsumish to About page

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,8 +3,10 @@ import { Route } from 'react-router-dom';
 
 const About = () => (
   <Route path="/about" render={() => (
-    <div>
+    <div className="gallery-container">
       <h2>About</h2>
+      <p>Exercitation occaecat eu dolor duis duis mollit exercitation ullamco voluptate sint sit eu anim do. Occaecat dolor ad labore deserunt tempor minim ad ex cillum enim reprehenderit occaecat deserunt. Magna veniam ipsum dolore labore voluptate magna eu velit labore est. Sint magna labore aliqua ullamco anim irure enim nostrud in tempor exercitation aliquip. Fugiat sint anim qui esse velit aute mollit nulla sint non. Commodo quis nostrud velit reprehenderit culpa. Deserunt et nulla commodo mollit nisi reprehenderit irure tempor adipisicing do.</p>
+      <p>Elit ipsum ea exercitation proident aute reprehenderit labore. Occaecat aute ea non adipisicing culpa duis ipsum esse. Laboris Lorem aliquip qui est ut eu mollit proident elit quis est. Dolor amet sint duis elit proident consequat laborum. Pariatur dolore qui Lorem ut.</p>
     </div>
   )} />
 );  
