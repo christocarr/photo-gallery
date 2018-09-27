5994e7be8d5fa26eb2fca5d5ff365ea06123107c
Chris <christocarr@outlook.com>
2018-09-27 20:48:41 +0100

    Add about page text

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,10 +3,11 @@ import { Route } from 'react-router-dom';
 
 const About = () => (
   <Route path="/about" render={() => (
-    <div className="gallery-container">
+    <div className="about-container">
       <h2>About</h2>
-      <p>Exercitation occaecat eu dolor duis duis mollit exercitation ullamco voluptate sint sit eu anim do. Occaecat dolor ad labore deserunt tempor minim ad ex cillum enim reprehenderit occaecat deserunt. Magna veniam ipsum dolore labore voluptate magna eu velit labore est. Sint magna labore aliqua ullamco anim irure enim nostrud in tempor exercitation aliquip. Fugiat sint anim qui esse velit aute mollit nulla sint non. Commodo quis nostrud velit reprehenderit culpa. Deserunt et nulla commodo mollit nisi reprehenderit irure tempor adipisicing do.</p>
-      <p>Elit ipsum ea exercitation proident aute reprehenderit labore. Occaecat aute ea non adipisicing culpa duis ipsum esse. Laboris Lorem aliquip qui est ut eu mollit proident elit quis est. Dolor amet sint duis elit proident consequat laborum. Pariatur dolore qui Lorem ut.</p>
+      <p>The photos shown on this website is the result of the passion I have for photography,  and travelling I did over the past decade or so. Born and raised in South Africa I came to the UK looking for better opportunities and a better quality of life. Long story short I worked, saved my hard earned money and did some travelling for a few years.</p>
+      <p>I tried to start a photography business doing, portraits for individuals and groups like family but found the market very saturated and it was tough getting it off the ground.  To showcase  my work I built my first website www.christophercarrphotography.co.uk being the first website i built is was not and easy process but it was very enjoyable. Seeing the finished product gave me huge sense of achievement.</p>
+      <p>This new website isn't just about photography but also of web development - a demonstration of the skills I learnt over the years. Being a self taught front end web developer </p>
     </div>
   )} />
 );  
