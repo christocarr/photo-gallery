24c9967a2614d22547e348a3f02409e74f7f8b1c
Chris <christocarr@outlook.com>
2018-09-29 14:20:23 +0100

    Fix link to external photography site

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ const About = () => (
       <h2>About Chris</h2>
       <p>Welcome to my photography portfolio. The photos shown in the galleries is the result of the passion I have for photography.</p>
       
-      <p>To showcase my work I built my first website <a href="www.christophercarrphotography.co.uk">christophercarrphotography.co.uk</a> It was challenging but I enjoyed the process, and seeing the finished product gave me huge sense of achievement.</p>
+      <p>To showcase my work I built my first website <a href="http://www.christophercarrphotography.co.uk">christophercarrphotography.co.uk</a> It was challenging but I enjoyed the process, and seeing the finished product gave me huge sense of achievement.</p>
       <p>This website isn't just about photography it's also about my journey becoming a web developer - a demonstration of the skills I learnt over the years. Being an aspiring front end web developer I thought it was the right time to spruce up my old photography site. </p>
       <p>I'm always looking for volunteering opportunities in photography and front end web development. So, get in touch if you would like to work with me.</p>
      
