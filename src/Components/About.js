04e131815fb183c007fe956c70f3a3b7c618549a
christocarr <christocarr@outlook.com>
2018-10-15 20:38:02 +0100

    ÂAss and style social media logos

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,7 +17,10 @@ export default class About extends Component {
           <p>To showcase my work I built my first website <a href="http://www.christophercarrphotography.co.uk">christophercarrphotography.co.uk</a> It was challenging but I enjoyed the process, and seeing the finished product gave me huge sense of achievement.</p>
           <p>This website isn't just about photography it's also about my journey becoming a web developer - a demonstration of the skills I learnt over the years. Being an aspiring front end web developer I thought it was the right time to spruce up my old photography site. </p>
           <p>I'm always looking for volunteering opportunities in photography and front end web development. So, get in touch if you would like to work with me.</p>
-         
+          <div className="icon-set">
+            <a href="https://www.instagram.com/christocarrgrapher/"><img src={require('../images/ig-glyph-logo.png')} alt="instagram logo" width="40px" height="40px"></img></a>
+            <a href="https://twitter.com/ChrisofCarr?lang=en"><img src={require('../images/twitter-logo.png')} alt="twitter logo" width="40px" height="40px"></img></a>
+          </div>
         </div>
       )} />
     )
