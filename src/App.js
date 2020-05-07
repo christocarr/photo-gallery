8766b6fdd3c109cb8147413c6b97af78607ffac0
Chris Carr <christocarr@outlook.com>
2020-05-07 14:38:40 +0100

    Fix component path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 class App extends Component {
 
