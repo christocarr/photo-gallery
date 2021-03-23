bd5c9be62073e92c9b57a462cead8ce08fd38ae4
christocarr <christocarr@outlook.com>
2021-03-23 13:44:56 +0000

    fix: menu hiding under image adjust z-index

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -1,7 +1,7 @@
 function MobileNav({ isOpen, children }) {
   return (
     isOpen && (
-      <nav className="absolute top-11 right-4 bg-white p-5 border">
+      <nav className="absolute z-30 top-11 right-4 bg-white p-5 border">
         <ul className="text-black">{children}</ul>
       </nav>
     )
