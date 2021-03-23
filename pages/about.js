5a28c05c028160979c34fc66a2a0b4a22a719c9c
christocarr <christocarr@outlook.com>
2021-03-23 11:51:17 +0000

    wip: add content to about page

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,45 @@ import Layout from '../components/Layout';
 export default function AboutPage() {
   return (
     <Layout>
-      <h2>About</h2>
+      <h2 className="text-lg mb-6 md:text-xl md:my-6">About</h2>
+      <p className="mb-4 leading-6 md:text-lg md:leading-7">
+        From the age of thirteen, I first realised my interest in photography
+        paging through numerous editions of National Geographic magazines
+        totally engrossed and captivated in the photographic images.
+      </p>
+
+      <p className="mb-4 leading-6 md:text-lg md:leading-7">
+        Living in vibrant and busy London, I continue to explore numerous
+        photographic techniques whilst taking advantage of the fast pace of
+        change, interesting subjects and urban architecture that the city has to
+        offer.
+      </p>
+
+      <p className="mb-4 leading-6 md:text-lg md:leading-7">
+        In the past five years, I have developed a keen eye and natural feel for
+        abstract, nature, travel and urban landscape photography and am looking
+        to use the skills I have learnt to choose a photography genre that
+        agrees to me.
+      </p>
+
+      <p className="mb-4 leading-6 md:text-lg md:leading-7">
+        Offering amateur and aspiring models the opportunity to obtain a
+        professional looking portfolio by providing ‘time for portfolio’ (TFP)
+        shoots, I intend to highlight the model's unique qualities and to
+        capture the viewers interest with minimum photographic software
+        manipulation whilst utilizing my skills as a portrait photographer. Go
+        to people in my galleries to see the work I have done with some amateur
+        models.
+      </p>
+
+      {/* <p className="mb-4 leading-6 md:text-lg md:leading-7">
+        Working for myself as a freelancer or for a publication, I continue to
+        work at improving my capabilities to become a professional photographer.
+        Whether as a travel photographer capturing images of the local and
+        indigenous people and animals that live in the countries I visit, or
+        continue working with people as a portrait photographer its become a
+        long term goal to achieve this dream.
+      </p> */}
     </Layout>
   );
 }
