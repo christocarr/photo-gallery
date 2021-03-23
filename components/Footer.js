204a00aea71f305fc05cea4f3612dd78228d1eda
christocarr <christocarr@outlook.com>
2021-03-23 12:19:35 +0000

    wip: add content and modify layout

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,12 @@
 function Footer() {
   return (
-    <footer className="absolute bottom-0 right-0 w-screen flex flex-col justify-center items-center h-12 md:py-12 xl:border-t">
-      <p>Copyright stuff</p>
+    <footer className="absolute bottom-0 right-0 w-screen flex flex-col justify-center items-center h-auto py-2 md:py-2 xl:border-t 2xl:flex-row place-content-center 2xl:py-6">
+      <p className="mb-4 leading-6 md:text-lg md:leading-7 2xl:mr-2 2xl:mb-0">
+        Copyright <span>&#169;</span> Christopher Carr
+      </p>
+      <p className="mb-4 leading-6 md:text-lg md:leading-7 2xl:mb-0">
+        Made with Next.js
+      </p>
     </footer>
   );
 }
