994878ef0342e57fd863e648195a2537d80bc6a3
christocarr <christocarr@outlook.com>
2021-03-23 13:43:49 +0000

    wip: add image for hero section

---
+++ images/hero.js
@@ -0,0 +1,10 @@
+export const tigerHero = [
+  {
+    src: '/assets/images/hero/tiger2.jpg',
+    // sizes: ['(min-width: 480px) 50vw,(min-width:1024px) 33.3vw, 100vw'],
+    width: 2,
+    height: 1.5,
+    key: '0',
+    alt: 'Tiger',
+  },
+];
