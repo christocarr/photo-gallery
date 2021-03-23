ca0f5eed1abb183c3196487cd23d16897d374deb
christocarr <christocarr@outlook.com>
2021-03-23 13:45:25 +0000

    wip: add content to index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from '../components/layout';
+import PhotoGallery from '../components/PhotoGallery';
+import { tigerHero } from '../images/hero';
 
 export default function IndexPage() {
   return (
     <Layout>
-      <h2>Index page</h2>
+      <PhotoGallery photos={tigerHero} />
+
+      <figure className="absolute z-50 w-48 -mt-8 right-2 py-4 px-6 border shadow-lg bg-white sm:-mt-32 lg:right-16 2xl:-right-4">
+        <blockquote>
+          <p>
+            "Great photography is about depth of feeling, not depth of field."
+          </p>
+        </blockquote>
+        <figcaption>- Peter Adams</figcaption>
+      </figure>
     </Layout>
   );
 }
